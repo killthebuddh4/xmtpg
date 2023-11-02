45fac1b96598278e975289402e7803673e150290
achilles schmelzer <achilles@daopanel.com>
2023-11-01 17:04:52 -0700

    Add failing test and test script

---
+++ index.test.js
@@ -0,0 +1,6 @@
+describe("index.js works", () => {
+  it("Prints Hello,world!", () => {
+    // Oh no, this is a problem, how do we know whether it printed?
+    throw new Error("Oh no, how do we test the console?");
+  });
+});
